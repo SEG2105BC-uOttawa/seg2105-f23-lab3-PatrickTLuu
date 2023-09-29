a73b1b3be99ed19f956a3f8e958f38cf8602a9b0
Patrick Luu <pluu079@uottawa.ca>
2023-09-29 10:06:03 -0400

    Modify code to have one method for numbers and one for operators

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.simplecalculator;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 
@@ -19,97 +18,77 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_layout);
     }
 
-    public void btn00Click(View view) {
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"0");
+    public void btnNumberClick(View view) {
+        int pressID = view.getId();
+        EditText eText = findViewById(R.id.resultEdit);
+        String newText = "";
+
+        if (pressID == R.id.btn00) {
+            newText = "0";
+        }
+        else if (pressID == R.id.btn01) {
+            newText = "1";
+        }
+        else if (pressID == R.id.btn02) {
+            newText = "2";
+        }
+        else if (pressID == R.id.btn03) {
+            newText = "3";
+        }
+        else if (pressID == R.id.btn04) {
+            newText = "4";
+        }
+        else if (pressID == R.id.btn05) {
+            newText = "5";
+        }
+        else if (pressID == R.id.btn06) {
+            newText = "6";
+        }
+        else if (pressID == R.id.btn07) {
+            newText = "7";
+        }
+        else if (pressID == R.id.btn08) {
+            newText = "8";
+        }
+        else if (pressID == R.id.btn09) {
+            newText = "9";
+        }
+        else if (pressID == R.id.btnDot) {
+            newText = ".";
+        }
+
+        eText.setText(String.format("%s%s", eText.getText(), newText));
     }
 
-    public void btn01Click(View view) {
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"1");
-    }
+    public void btnOperatorClick(View view) {
+        int pressID = view.getId();
+        EditText eText = findViewById(R.id.resultEdit);
 
-    public void btn02Click(View view) {
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"2");
-    }
+        if (pressID == R.id.btnAdd) {
+            operator = Operator.add;
+        }
+        else if (pressID == R.id.btnMinus) {
+            operator = Operator.minus;
+        }
+        else if (pressID == R.id.btnMultiply) {
+            operator = Operator.multiply;
+        }
+        else if (pressID == R.id.btnDivide) {
+            operator = Operator.divide;
+        }
 
-    public void btn03Click(View view) {
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"3");
-    }
-
-    public void btn04Click(View view) {
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"4");
-    }
-
-    public void btn05Click(View view) {
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"5");
-    }
-
-    public void btn06Click(View view) {
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"6");
-    }
-
-    public void btn07Click(View view) {
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"7");
-    }
-
-    public void btn08Click(View view) {
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"8");
-    }
-
-    public void btn09Click(View view) {
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"9");
-    }
-
-    public void btnAddClick(View view) {
-        operator = Operator.add;
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        data1 = Double.parseDouble(eText.getText().toString());
-        eText.setText("");
-    }
-
-    public void btnMinusClick(View view) {
-        operator = Operator.minus;
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        data1 = Double.parseDouble(eText.getText().toString());
-        eText.setText("");
-    }
-
-    public void btnMultiplyClick(View view) {
-        operator = Operator.multiply;
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        data1 = Double.parseDouble(eText.getText().toString());
-        eText.setText("");
-    }
-
-    public void btnDivideClick(View view) {
-        operator = Operator.divide;
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
         data1 = Double.parseDouble(eText.getText().toString());
         eText.setText("");
     }
 
     public void btnClearClick(View view) {
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        EditText eText = findViewById(R.id.resultEdit);
         eText.setText("");
-    }
-
-    public void btnDotClick(View view) {
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+".");
     }
 
     public void btnResultClick(View view) {
         if (operator != Operator.none) {
-            EditText eText = (EditText)findViewById(R.id.resultEdit);
+            EditText eText = findViewById(R.id.resultEdit);
             data2 = Double.parseDouble(eText.getText().toString());
 
             double result = 0;
